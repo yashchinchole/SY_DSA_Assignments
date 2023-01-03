90422c1f263917bf6073dd723b5732a05f2e87ac
YASH <iamyash337@gmail.com>
2023-01-03 08:24:54 +0530

    main

--- Assignment5.cpp
+++ Assignment5.cpp
@@ -1,94 +1,116 @@
-// In one-way threaded binary trees, a thread will appear either in the right or left link field of a node. If it appears in the right link field of a node then it will point to the next node that will appear on performing in order traversal. Such trees are called Right threaded binary trees. If thread appears in the left field of a node then it will point to the nodes inorder predecessor. Such trees are called Left threaded binary trees. Left threaded binary trees are used less often as they don't yield the last advantages of right threaded binary trees. In one-way threaded binary trees, the right link field of last node and left link field of first node contains a NULL. In order to distinguish threads from normal links they are represented by dotted lines.
-
-// https://www.javatpoint.com/threaded-binary-tree
+// ASSIGNMENT NO - 5
+// NAME - YASH JITENDRA CHINCHOLE
+// ROLL NO - SYCOA37
 
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node
+class Node
 {
+public:
     int data;
     Node *left, *right;
-    bool rightThread;
+    bool LeftThread, RightThread;
 };
 
-Node *Newnode(int data)
+class TBT
 {
-    Node *temp = new Node;
-    temp->data = data;
-    temp->left = temp->right = NULL;
-    return temp;
+private:
+    Node *root;
+
+public:
+    TBT();
+    void Insert(int);
+    void InOrder();
+};
+
+TBT::TBT()
+{
+    root = new Node;
+    root->right = root->left = root;
+    root->LeftThread = true;
+    root->data = INT_MAX;
 }
 
-struct Node *leftMost(struct Node *n)
+void TBT::Insert(int key)
 {
-    if (n == NULL)
-        return NULL;
+    Node *p = root;
 
-    while (n->left != NULL)
-        n = n->left;
-
-    return n;
-}
-
-void inOrder(struct Node *root)
-{
-    struct Node *cur = leftMost(root);
-    while (cur != NULL)
+    while (1)
     {
-        cout << cur->data;
+        if (p->data < key)
+        {
+            if (p->RightThread)
+                break;
+            p = p->right;
+        }
 
-        if (cur->rightThread)
-            cur = cur->right;
-        // else
-            // cur = leftmost(cur->right);
+        else if (p->data > key)
+        {
+            if (p->LeftThread)
+                break;
+            p = p->left;
+        }
+
+        else
+            return;
+    }
+
+    Node *temp = new Node;
+    temp->data = key;
+    temp->RightThread = temp->LeftThread = true;
+
+    if (p->data < key)
+    {
+        temp->right = p->right;
+        temp->left = p;
+        p->right = temp;
+        p->RightThread = false;
+    }
+
+    else
+    {
+        temp->right = p;
+        temp->left = p->left;
+        p->left = temp;
+        p->LeftThread = false;
     }
 }
 
-void Inorder(struct Node *node)
+void TBT::InOrder()
 {
-    if (node == NULL)
-        return;
-
-    Inorder(node->left);
-    cout << node->data << " ";
-    Inorder(node->right);
-}
-
-void Preorder(struct Node *node)
-{
-    if (node == NULL)
-        return;
-
-    cout << node->data << " ";
-    Preorder(node->left);
-    Preorder(node->right);
-}
-
-void Postorder(struct Node *node)
-{
-    if (node == NULL)
-        return;
-
-    Postorder(node->left);
-    Postorder(node->right);
-    cout << node->data << " ";
+    Node *temp = root, *p;
+    for (;;)
+    {
+        p = temp;
+        temp = temp->right;
+        if (!p->RightThread)
+        {
+            while (!temp->LeftThread)
+            {
+                temp = temp->left;
+            }
+        }
+        if (temp == root)
+            break;
+        cout << temp->data << " ";
+    }
+    cout << endl;
 }
 
 int main()
 {
-    struct Node *root = Newnode(1);
-    root->left = Newnode(2);
-    root->right = Newnode(3);
-    root->left->left = Newnode(4);
-    root->left->right = Newnode(5);
-
-    cout << "Inorder : ";
-    Inorder(root);
-    cout << "\nPreorder : ";
-    Preorder(root);
-    cout << "\nPostorder : ";
-    Postorder(root);
+    TBT tbt;
+    cout << "Threaded Binary Tree" << endl;
+    tbt.Insert(20);
+    tbt.Insert(10);
+    tbt.Insert(30);
+    tbt.Insert(5);
+    tbt.Insert(16);
+    tbt.Insert(14);
+    tbt.Insert(17);
+    tbt.Insert(13);
+    tbt.InOrder();
 
     return 0;
 }
